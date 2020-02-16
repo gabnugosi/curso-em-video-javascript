384e4706181aee8787d4cccd53b148e9c7d8a62d
Gabriel Nunes <nunes.gabriel.13@gmail.com>
2020-02-16 15:36:52 -0300

    Commit aula 11

---
+++ aula0011/ex007.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60)
+    console.log('Parabéns você foi MULTADO, o limite era 60')    
+console.log('Dirija sempre usando um cinto de segurança')
